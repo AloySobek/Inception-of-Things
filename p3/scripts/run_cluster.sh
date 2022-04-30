d8e24bed2738a8618e77e893e0f9b756172f0fe5
Margarita Utochkina <rimargri@gmail.com>
2022-04-30 20:03:36 +0300

    p3: add folders

--- p3/scripts/run_cluster.sh
+++ p3/scripts/run_cluster.sh
@@ -3,4 +3,4 @@ k3d cluster create iothings -p "8081:80@loadbalancer" --agents 2
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl create namespace dev
-kubectl apply -f wil42-config.yaml
+kubectl apply -f ../confs/wil42-config.yaml
