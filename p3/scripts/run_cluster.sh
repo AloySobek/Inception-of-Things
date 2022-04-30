e8fa4ade23b0e70438724a36c061d2ab15ec1b58
MikhailPasechnik <michail.goodchild@gmail.com>
2022-04-30 23:21:58 +0300

    pt3: sync options create ns added

--- p3/scripts/run_cluster.sh
+++ p3/scripts/run_cluster.sh
@@ -2,5 +2,4 @@ k3d cluster delete -a
 k3d cluster create iothings -p "8081:80@loadbalancer" --agents 2
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl create namespace dev
 kubectl apply -f ../confs/wil42-config.yaml
