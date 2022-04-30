cb8e2c7c5c5b7dde83dfe250afaa8cb89aa630c3
Margarita Utochkina <rimargri@gmail.com>
2022-04-30 19:22:15 +0300

    p3: add config and commands

---
+++ p3/commands.sh
@@ -0,0 +1,6 @@
+k3d cluster delete -a
+k3d cluster create iothings -p "8081:80@loadbalancer" --agents 2
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl create namespace dev
+kubectl apply -f wil42-config.yaml
