a43733a6fa37d065a9fa0406e648969367a02183
MikhailPasechnik <michail.goodchild@gmail.com>
2022-04-30 23:23:42 +0300

    p3: chmod and shibang

--- p3/scripts/run_cluster.sh
+++ p3/scripts/run_cluster.sh
@@ -1,4 +1,5 @@
-k3d cluster delete -a
+#!/bin/bash
+k3d cluster delete iothings
 k3d cluster create iothings -p "8081:80@loadbalancer" --agents 2
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
