565361306364c0606b246fa5ba707d57943b8911
MikhailPasechnik <michail.goodchild@gmail.com>
2022-05-01 20:47:56 +0300

    p3: development project and wait for argocd depl in run cluster

--- p3/scripts/run_cluster.sh
+++ p3/scripts/run_cluster.sh
@@ -3,4 +3,5 @@ k3d cluster delete iothings
 k3d cluster create iothings -p "8081:80@loadbalancer" --agents 2
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl wait --for=condition=available --timeout=9000s deployment/argocd-server -n argocd
 kubectl apply -f ../confs/wil42-config.yaml
