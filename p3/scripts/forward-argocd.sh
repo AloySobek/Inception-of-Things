70db6b1cd906e3a1b63a5dbcb31c3414ae51866c
AloySobek <supernova.wandering.star@gmail.com>
2022-05-01 18:36:34 +0300

    feat(p3): kubectl port-forward with 0.0.0.0 bind

--- p3/scripts/forward-argocd.sh
+++ p3/scripts/forward-argocd.sh
@@ -2,4 +2,4 @@
 echo "Username: admin"
 echo "Password: $(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 --decode)"
 echo "https://localhost:4443"
-kubectl port-forward service/argocd-server 4443:443 -n argocd
+kubectl port-forward --address 0.0.0.0 service/argocd-server 4443:443 -n argocd
